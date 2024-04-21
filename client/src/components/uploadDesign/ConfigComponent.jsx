e8130f677cb1b735ec20d336c170e4be4765c63f
umer <umer.mahmood@dci-student.org>
2024-04-21 22:26:30 +0200

    Update Design Comp

--- client/src/components/uploadDesign/ConfigComponent.jsx
+++ client/src/components/uploadDesign/ConfigComponent.jsx
@@ -1,4 +1,136 @@
 
+
+// import React, { useState, useEffect } from "react";
+// import Resolution from "./Resolution";
+// import Resistance from "./Resistance";
+// import QuoteSummary from "./QuoteSummary";
+// import Material from "./Materials";
+// import ColorSelector from "./ColorSelector";
+// import "../../styling/quoteSummary.css";
+// import "../../styling/configComponent.css";
+// import UploadFile from "./UploadFile";
+// import Navbar from "../../components/Navbar.jsx";
+// import Footer from "../../components/Footer.jsx";
+
+// const Popup = ({ material, color, resolution, resistance, onClose }) => {
+// 	return (
+// 		<div className="popup-container">
+// 			<div className="popup" onClick={(e) => e.stopPropagation()}>
+// 				<QuoteSummary
+// 					material={material}
+// 					color={color}
+// 					resolution={resolution}
+// 					resistance={resistance}
+// 				/>
+// 				<div className="popupwindow-button">
+// 					<button className="close-btn" onClick={onClose}>
+// 						Close
+// 					</button>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// const ConfigComponent = () => {
+// 	const [selectedMaterial, setSelectedMaterial] = useState("ABS");
+// 	const [selectedColor, setSelectedColor] = useState("");
+// 	const [selectedResolution, setSelectedResolution] = useState("");
+// 	const [selectedResistance, setSelectedResistance] = useState("");
+// 	const [showPopup, setShowPopup] = useState(false);
+
+// 	const handleMaterialChange = (e) => {
+// 		setSelectedMaterial(e.target.value);
+// 	};
+
+// 	const handleColorSelection = (color) => {
+// 		setSelectedColor(color);
+// 	};
+
+// 	const handleResistanceSelection = (resistance) => {
+// 		setSelectedResistance(resistance);
+// 	};
+
+// 	const handleResolutionSelection = (resolution) => {
+// 		setSelectedResolution(resolution);
+// 	};
+
+// 	const handleSendQuote = () => {
+// 		setShowPopup(true);
+// 	};
+
+// 	const handleClosePopup = () => {
+// 		setShowPopup(false);
+// 		setSelectedColor("");
+// 		setSelectedResolution("");
+// 		setSelectedResistance("");
+// 		setSelectedMaterial("ABS");
+// 	};
+
+// 	useEffect(() => {
+// 		const handleClickOutside = (event) => {
+// 			if (showPopup && !event.target.closest(".popup")) {
+// 				setShowPopup(false);
+// 			}
+// 		};
+
+// 		document.addEventListener("mousedown", handleClickOutside);
+
+// 		return () => {
+// 			document.removeEventListener("mousedown", handleClickOutside);
+// 		};
+// 	}, [showPopup]);
+
+// 	return (
+// 		<>
+// 		<Navbar/>
+// 			<div className="config-container">
+// 				<div>
+// 					<Material
+// 						selectedMaterial={selectedMaterial}
+// 						handleMaterialChange={handleMaterialChange}
+// 					/>
+
+// 					<ColorSelector
+// 						selectedColor={selectedColor}
+// 						handleColorSelection={handleColorSelection}
+// 					/>
+// 					<Resolution
+// 						selectedResolution={selectedResolution}
+// 						handleResolutionSelection={handleResolutionSelection}
+// 					/>
+// 					<Resistance
+// 						selectedResistance={selectedResistance}
+// 						handleResistanceSelection={handleResistanceSelection}
+// 					/>
+// 				</div>
+
+// 				<div className="upload-file">
+// 					<UploadFile/>
+// 				</div>
+// 			</div>
+
+// 			{!showPopup ? (
+// 				<button onClick={handleSendQuote}>Send Quote</button>
+// 			) : null}
+
+// 			{showPopup && (
+// 				<Popup
+// 					material={selectedMaterial}
+// 					color={selectedColor}
+// 					resolution={selectedResolution}
+// 					resistance={selectedResistance}
+// 					onClose={handleClosePopup}
+// 				/>
+// 			)}
+
+// 			<Footer/>
+// 		</>
+// 	);
+// };
+
+// export default ConfigComponent;
+
 import React, { useState, useEffect } from "react";
 import Resolution from "./Resolution";
 import Resistance from "./Resistance";
@@ -6,7 +138,10 @@ import QuoteSummary from "./QuoteSummary";
 import Material from "./Materials";
 import ColorSelector from "./ColorSelector";
 import "../../styling/quoteSummary.css";
+import "../../styling/configComponent.css";
 import UploadFile from "./UploadFile";
+import Navbar from "../../components/Navbar.jsx";
+import Footer from "../../components/Footer.jsx";
 
 const Popup = ({ material, color, resolution, resistance, onClose }) => {
 	return (
@@ -60,7 +195,7 @@ const ConfigComponent = () => {
 		setSelectedColor("");
 		setSelectedResolution("");
 		setSelectedResistance("");
-		setSelectedMaterial("ABS")
+		setSelectedMaterial("ABS");
 	};
 
 	useEffect(() => {
@@ -79,31 +214,39 @@ const ConfigComponent = () => {
 
 	return (
 		<>
-			<div>
-				<Material
-					selectedMaterial={selectedMaterial}
-					handleMaterialChange={handleMaterialChange}
-				/>
+			<Navbar/>
+			<div className="config-container">
+				<div className="left-div">
+					<Material
+						selectedMaterial={selectedMaterial}
+						handleMaterialChange={handleMaterialChange}
+					/>
 
-				<ColorSelector
-					selectedColor={selectedColor}
-					handleColorSelection={handleColorSelection}
-				/>
-				<Resolution
-					selectedResolution={selectedResolution}
-					handleResolutionSelection={handleResolutionSelection}
-				/>
-				<Resistance
-					selectedResistance={selectedResistance}
-					handleResistanceSelection={handleResistanceSelection}
-				/>
+					<ColorSelector
+						selectedColor={selectedColor}
+						handleColorSelection={handleColorSelection}
+					/>
+					<Resolution
+						selectedResolution={selectedResolution}
+						handleResolutionSelection={handleResolutionSelection}
+					/>
+					<Resistance
+						selectedResistance={selectedResistance}
+						handleResistanceSelection={handleResistanceSelection}
+					/>
+				</div>
 
-				<UploadFile/>
+				<div className="right-div">
+					<div className="upload-file">
+						<UploadFile />
+					</div>
+				</div>
 			</div>
 
-			{!showPopup ? (
+<div className="button-quote">{!showPopup ? (
 				<button onClick={handleSendQuote}>Send Quote</button>
-			) : null}
+			) : null}</div>
+			
 
 			{showPopup && (
 				<Popup
@@ -114,8 +257,11 @@ const ConfigComponent = () => {
 					onClose={handleClosePopup}
 				/>
 			)}
+
+			<Footer />
 		</>
 	);
 };
 
 export default ConfigComponent;
+
