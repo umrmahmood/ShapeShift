017070247d01cda87c32069b40f38bf5e53473d8
Eugen Taranowski <135104501+EugeneeT@users.noreply.github.com>
2024-04-23 10:28:44 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,33 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
 
 import Navbar from "./components/Navbar";
 import Main from "./components/Main2";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
-import Cart from "./components/Cart";
-
-
-
-
-import ConfigComponent from "./components/uploadDesign/ConfigComponent.jsx";
+import ItemPage from "./CardComponent/ItemPage";
+import UserSellCard from "./CardComponent/UserSellCard";
+import Printer from "./components/Printer";
 
 function App() {
 	return (
-		<div className="App">
+    <Router>
+      <div className="App">
 			<header className="App-header">
 				<Navbar />
 			</header>
-			<Main />
-			
-			<Features />
-			<Cart />
+      <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/features" element={<Features />} />
+      <Route path="/item" element={<ItemPage />} />
+      <Route path="/home" element={<UserSellCard/>} />
+      <Route path="/printers" element={<Printer/>} />
+      </Routes>
+
 			<Footer />
-	
-			
-			
-			<ConfigComponent/>
 		</div>
+    </Router>
+
 	);
 }
-
-export default App;
