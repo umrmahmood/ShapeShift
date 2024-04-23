61a114bc268231f1a6863c068260700c1fd491d7
NadiNadiya <135104478+NadiNadiya@users.noreply.github.com>
2024-04-23 10:35:34 +0200

    change app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,19 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Navbar from "./components/Navbar";
 import Main from "./components/Main2";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
-import ItemPage from "./CardComponent/ItemPage";
-import UserSellCard from "./CardComponent/UserSellCard";
-import Printer from "./components/Printer";
-
 function App() {
 	return (
-    <Router>
-      <div className="App">
+		<div className="App">
 			<header className="App-header">
 				<Navbar />
 			</header>
-      <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/features" element={<Features />} />
-      <Route path="/item" element={<ItemPage />} />
-      <Route path="/home" element={<UserSellCard/>} />
-      <Route path="/printers" element={<Printer/>} />
-      </Routes>
-
+			<Main />
+			<Features />
 			<Footer />
 		</div>
-    </Router>
-		
 	);
 }
-
 export default App;
