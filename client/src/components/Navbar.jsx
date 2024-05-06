fbc3aedb6d8031045defcacd109ccd69fe8a65ab
umer <umer.mahmood@dci-student.org>
2024-05-06 14:39:39 +0200

    Update Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,70 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { faShoppingBag, faEarth, faShop } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faBell,
+  // faSearch,
+} from "@fortawesome/free-regular-svg-icons";
+
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/SSlogo.png";
+import "../styling/navbar.css";
 
 const Navbar = ({ onLoginClick }) => {
-	const [searchQuery, setSearchQuery] = useState("");
-	const navigate = useNavigate(); // Initialize useNavigate
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const navigate = useNavigate();
 
-	const handleSearchInputChange = (event) => {
-		setSearchQuery(event.target.value);
-	};
+  const tokenFromLocalStorage = localStorage.getItem("shapeshiftkey");
 
-	const handleShoppingBagClick = () => {
-		navigate("/cart"); // Navigate to the cart component
-	};
+  useEffect(() => {
+    const token = localStorage.getItem("shapeshiftkey");
+    setIsLoggedIn(!!token); 
+  }, [tokenFromLocalStorage]); 
 
-	return (
-		<>
-			<div className="Navbar-container">
-				<div className="Logo">
-					<a href="/">
-						<img src={logo} alt="logo" />
-					</a>
-				</div>
-				<ul className="Navbar">
-					<li>
-						<a href="#">HOME</a>
-					</li>
-					<li>
-						<a href="#">ABOUT US</a>
-					</li>
-					<li>
-						<a href="#">CONTACT US</a>
-					</li>
-					<li>
-						<a href="#">Sell</a>
-					</li>
-				</ul>
-				<div className="nav-icons">
-					<li>
-						<input
-							type="text"
-							value={searchQuery}
-							onChange={handleSearchInputChange}
-							placeholder="Search"
-						/>
-					</li>
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-					<li className="shoppingbag" onClick={handleShoppingBagClick}>
-						{" "}
-						{/* Add onClick event */}
-						<FontAwesomeIcon icon={faShoppingBag} />
-					</li>
-					<li>
-						<a href="/">
-							<FontAwesomeIcon icon={faUser} />
-						</a>
-					</li>
-					<li className="navbar-login-btn">
-						<button onClick={onLoginClick}>Login</button>
-					</li>
-				</div>
-			</div>
-		</>
-	);
+  const handleShoppingBagClick = () => {
+    navigate("/cart");
+  };
+
+  return (
+    <>
+      <div className="Navbar-container">
+        <div className="nav-logo">
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+        <div className="nav-search">
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+            placeholder="Search for anything"
+          />
+        </div>
+
+        <div className="nav-icons">
+          <li className="navbar-login-btn">
+            <button onClick={onLoginClick}>{isLoggedIn ? "Sell" : "Login"}</button>
+          </li>
+
+          <li>
+            <a href="/">
+              <FontAwesomeIcon icon={faEarth} />
+            </a>
+          </li>
+
+          {isLoggedIn && (
+            <>
+              <li>
+                <a href="/">
+                  <FontAwesomeIcon icon={faBell} />
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <FontAwesomeIcon icon={faUser} />
+                </a>
+              </li>
+			  <li>
+                <a href="/">
+                  <FontAwesomeIcon icon={faShop} />
+                </a>
+              </li>
+            </>
+			
+          )}
+
+          <li className="shoppingbag" onClick={handleShoppingBagClick}>
+            <FontAwesomeIcon icon={faShoppingBag} />
+          </li>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
+
