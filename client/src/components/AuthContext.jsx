2c5e0f986b946f683133c7078ef7193a80dadb17
EugeneeT <vina.botanic@gmail.com>
2024-06-03 22:21:15 +0200

    all features in chat working now

--- client/src/components/AuthContext.jsx
+++ client/src/components/AuthContext.jsx
@@ -1,16 +1,16 @@
-// AuthContext.jsx
-
 import React, { createContext, useState, useEffect } from "react";
-import { auth, fireDB } from "./Firebase.jsx";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { fireDB } from "./Firebase.jsx";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    const setOnlineStatus = async (user) => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         const userRef = doc(fireDB, "users", user.uid);
         await setDoc(
@@ -19,35 +19,43 @@ export const AuthProvider = ({ children }) => {
           { merge: true }
         );
 
-        // Set offline status when the user disconnects
-        const onDisconnectRef = userRef;
-        await setDoc(
-          onDisconnectRef,
-          { online: false, lastOnline: serverTimestamp() },
-          { merge: true }
-        );
-      }
-    };
+        setCurrentUser(user);
 
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      if (user) {
-        setOnlineStatus(user);
+        window.addEventListener("beforeunload", async () => {
+          await setDoc(
+            userRef,
+            { online: false, lastOnline: serverTimestamp() },
+            { merge: true }
+          );
+        });
+
+        const idleCallback = async () => {
+          await setDoc(
+            userRef,
+            { online: false, lastOnline: serverTimestamp() },
+            { merge: true }
+          );
+        };
+
+        document.addEventListener("visibilitychange", idleCallback);
+
+        return () => {
+          document.removeEventListener("visibilitychange", idleCallback);
+          window.removeEventListener("beforeunload", async () => {
+            await setDoc(
+              userRef,
+              { online: false, lastOnline: serverTimestamp() },
+              { merge: true }
+            );
+          });
+        };
+      } else {
+        setCurrentUser(null);
       }
     });
 
-    return () => {
-      unsubscribe();
-      if (currentUser) {
-        const userRef = doc(fireDB, "users", currentUser.uid);
-        setDoc(
-          userRef,
-          { online: false, lastOnline: serverTimestamp() },
-          { merge: true }
-        );
-      }
-    };
-  }, [currentUser]);
+    return () => unsubscribe();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ currentUser }}>
